f1027671b81cc6467eb5486afb5b015af10c2d82
Colorfuls <mint.rosetta2001@gmail.com>
2024-02-21 01:12:08 +0700

    lecture - loop object

--- reactpro/src/components/Fruits.jsx
+++ reactpro/src/components/Fruits.jsx
@@ -1,9 +1,16 @@
 export default function Fruits() {
-    const fruits = ['Apple', 'Mango', 'Banana', 'Orange', 'Pine Apple'];
+    // const fruits = ['Apple', 'Mango', 'Banana', 'Orange', 'Pine Apple'];
+    const fruits = [
+        { name: 'Apple', price: 10, emoji: '🍎' },
+        { name: 'Mango', price: 15, emoji: '🥭' },
+        { name: 'Banana', price: 12, emoji: '🍌' },
+        { name: 'Orange', price: 13, emoji: '🍊' },
+        { name: 'Pineapple', price: 7, emoji: '🍍' }
+    ];
 
     return (
         <ul>
-            {fruits.map((item, index) => <li key={index}>{item}</li>)}
+            {fruits.map((item, index) => <li key={index}>{item.emoji} {item.name} {`$${item.price}`}</li>)}
         </ul>
     );
 }
